1cb472e5df5eda913db9021e901e312c19ea2825
nvpnathan <nness@nness-mbp.local>
2018-11-04 12:38:16 +0100

    added upgrade alias

--- pks-setup.sh
+++ pks-setup.sh
@@ -4,12 +4,14 @@
 CONCOURSE_ENDPOINT=fly.vballin.com:8080
 CONCOURSE_TARGET=nsx-concourse
 PIPELINE_NAME=install-pks-with-nsx
+UPGRADE_PIPELINE=upgrade-pks
 CONCOURSE_USER=nsx
 CONCOURSE_PW=vmware
 
 alias fly-s="fly -t $CONCOURSE_TARGET set-pipeline -p $PIPELINE_NAME -c install-pks-pipeline.yml -l pks-params.yml"
 alias fly-l="fly -t $CONCOURSE_TARGET containers | grep $PIPELINE_NAME"
 alias fly-h="fly -t $CONCOURSE_TARGET hijack -b "
+alias fly-u="fly -t $CONCOURSE_TARGET set-pipeline -p $UPGRADE_PIPELINE -c pks-upgrade/upgrade-pipeline.yml -l pks-upgrade/upgrade-params.yml"
 
 echo "Concourse target set to $CONCOURSE_ENDPOINT"
 echo "Login using fly"
