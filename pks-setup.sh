583a24497f0d07efe891d37199c610d5792dbaa3
nvpnathan <nathan79@gmail.com>
2019-01-17 13:52:01 -0600

    fixed syslog

--- pks-setup.sh
+++ pks-setup.sh
@@ -8,7 +8,8 @@ UPGRADE_PIPELINE=upgrade-pks
 CONCOURSE_USER=nsx
 CONCOURSE_PW=vmware
 
-alias fly-s="fly -t $CONCOURSE_TARGET set-pipeline -p $PIPELINE_NAME -c ../nsx-t-ci-pipeline/pipelines/install-pks-pipeline.yml -l ../nsx-t-ci-pipeline/pipelines/pks-params.yml"
+alias fly-s="fly -t $CONCOURSE_TARGET set-pipeline -p $PIPELINE_NAME -c ../nsx-t-ci-pipeline/pipelines/install-pks-pipeline.yml -l pks-params.yml"
+alias fly-d="fly -t $CONCOURSE_TARGET destroy-pipeline -p $PIPELINE_NAME" 
 alias fly-l="fly -t $CONCOURSE_TARGET containers | grep $PIPELINE_NAME"
 alias fly-h="fly -t $CONCOURSE_TARGET hijack -b "
 alias fly-u="fly -t $CONCOURSE_TARGET set-pipeline -p $UPGRADE_PIPELINE -c pks-upgrade/upgrade-pipeline.yml -l pks-upgrade/upgrade-params.yml"
